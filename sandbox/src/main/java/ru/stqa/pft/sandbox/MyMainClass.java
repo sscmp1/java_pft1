c66286fcb3d400b1340ea7a9e83c85d84c4b008d
marina.peunkova <marina.peunkova@billing.ru>
2016-04-16 17:37:48 +0300

    Задание 3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyMainClass.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyMainClass.java
@@ -4,4 +4,7 @@ package ru.stqa.pft.sandbox;
  * Created by marina.peunkova on 16/04/2016.
  */
 public class MyMainClass {
+    public static void main (String[] args){
+        System.out.println("Hello, world!");
+    }
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -4,4 +4,11 @@ package ru.stqa.pft.sandbox;
  * Created by marina.peunkova on 16/04/2016.
  */
 public class Square {
+    public int w;
+    Square(int w){
+        this.w=w;
+    }
+    public int area(){
+        return this.w*this.w;
+    }
 }
