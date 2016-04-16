2be8c26e47c7f41d4b923faabf1e4792077af04f
marina.peunkova <marina.peunkova@billing.ru>
2016-04-16 16:13:18 +0300

    initial

---
+++ sandbox/src/main/java/JavaFirst.java
@@ -0,0 +1,8 @@
+/**
+ * Created by marina.peunkova on 13/04/2016.
+ */
+public class JavaFirst {
+    public static void main (String[] args){
+        System.out.println("Hello, world!");
+    }
+}
